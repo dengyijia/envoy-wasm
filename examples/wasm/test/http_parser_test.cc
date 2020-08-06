33dbfa4077854c843ec2d63e79f88702e3f49a6c
Adela Deng <kagura@google.com>
2020-08-06 16:51:08 +0000

    add params matcher in http parser test

--- examples/wasm/test/http_parser_test.cc
+++ examples/wasm/test/http_parser_test.cc
@@ -7,12 +7,28 @@ using ::testing::Eq;
 using ::testing::UnorderedElementsAreArray;
 using ::testing::IsEmpty;
 
-TEST(HttpParserTest, PercentDecodingTest) {
+MATCHER_P(ParamsEq, expected_params, "Params are not equal") {
+  if (expected_params.size() != arg.size()) {
+    return false;
+  }
+  for (auto param : expected_params) {
+    auto match = arg.find(param.first);
+    if (match == arg.end() || match->second != param.second) {
+      return false;
+    }
+  }
+  return true;
+}
 
+TEST(HttpParserTest, PercentDecodingTest) {
+  ASSERT_EQ(percentDecode(""), "");
+  ASSERT_EQ(percentDecode("not-encoded"), "not-encoded");
 }
 
 TEST(HttpParserTest, ParseCookieTest) {
-
+  QueryParams params0 = parseCookie("");
+  QueryParams expected0 ({});
+  ASSERT_THAT(params0, ParamsEq(expected0));
 }
 
 TEST(HttpParserTest, ParseBodyTest) {
